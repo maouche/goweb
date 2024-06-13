974f92b1e30aeea356785d6acbd748a2be24d0a1
Yacine Maouche <ymaouche@kardham-digital.com>
2024-06-13 16:28:05 +0200

    add layout system to render pages html

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,20 +1,57 @@
 package handlers
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "home")
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	t, err := template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
+func renderTemplate(w http.ResponseWriter, tmplName string) {
+	templateCache, err := createTemplateCache()
+
 	if err != nil {
-		fmt.Fprintf(w, err.Error(), http.StatusInternalServerError)
+		panic(err)
 	}
 
-	t.Execute(w, nil)
+	tmpl, ok := templateCache[tmplName+".page.tmpl"]
+
+	if !ok {
+		http.Error(w, "Template not found.", http.StatusInternalServerError)
+		return
+	}
+
+	buffer := new(bytes.Buffer)
+	tmpl.Execute(buffer, nil)
+	buffer.WriteTo(w)
+}
+func createTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return cache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		tmpl := template.Must(template.ParseFiles(page))
+		layouts, err := filepath.Glob("./templates/layouts/*.layout.tmpl")
+
+		if err != nil {
+			return cache, err
+		}
+
+		if len(layouts) > 0 {
+			tmpl.ParseGlob("./templates/layouts/*.layout.tmpl")
+		}
+
+		cache[name] = tmpl
+	}
+
+	return cache, nil
 }
